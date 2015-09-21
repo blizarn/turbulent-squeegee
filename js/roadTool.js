37f8e8f0646dd6861bd9de05e76eebf4201fd532
Morgan H <cerril54@gmail.com>
2015-09-21 17:57:12 -0500

    Roads
    Roads no longer objects, all types can be placed. Everything is still
    visual nothing does anything.

--- js/roadTool.js
+++ js/roadTool.js
@@ -8,8 +8,7 @@ function RoadTool() {
         var cursor = Game.controls.Mouse.Pos;
         origin = cursor;
         active = true;
-        window.console.log(cursor);
-        var start = Game.Map.val[cursor[0]][cursor[1]];
+        Game.Map.val[cursor[0]][cursor[1]] = roadType.FOURWAY;
     };
     
     this.getActive = function getActive() {return active;};
@@ -34,76 +33,239 @@ function RoadTool() {
         
     };
     
-    var checkTiles = function checkTiles(x, y, map) {
-        var tileEast;
+    var checkTiles = function checkTiles(x, y, map, df, end) {
+        var config;
+        //East
         if (x < map.length-1) {
-            if (map[x+1][y].data !== undefined) tileEast = map[x+1][y].data.prototype;
+            if (map[x+1][y] === 0 || map[x+1][y] === 1) {
+                //None
+                config = 0;
+            } else {
+                //East
+                config = 1;
+            }
         }
-        var tileSouth;
+        //South
         if (y < map[0].length-1) {
-            if (map[x][y+1].data !== undefined) tileSouth = map[x][y+1].data.prototype;
+            if (map[x][y+1] === 0 || map[x][y+1] === 1) {
+                
+            } else {
+                switch(config) {
+                    case 0:
+                        //South
+                        config = 2;
+                        break;
+                    case 1:
+                        //South-East
+                        config = 5;
+                        break;
+                }
+            }
         }
-        var tileWest;
+        //West
         if (x > 0) {
-            if (map[x-1][y].data !== undefined) tileWest = map[x-1][y].data.prototype;
+            if (map[x-1][y] === 0 || map[x-1][y] === 1) {
+                
+            } else {
+                switch(config) {
+                    case 0:
+                        //West
+                        config = 3;
+                        break;
+                    case 1:
+                        //East-West
+                        config = 14;
+                        break;
+                    case 2:
+                        //South-West
+                        config = 6;
+                        break;
+                    case 5:
+                        //South-East-West
+                        config = 9;
+                        break;
+                }
+            }
         }
-        var tileNorth;
+        //North
         if (y > 0) {
-            if (map[x][y-1].data !== undefined) tileNorth = map[x][y-1].data.prototype;
+            if (map[x][y-1] === 0 || map[x][y-1] === 1) {
+                
+            } else {
+                switch(config) {
+                    case 0:
+                        //North
+                        config = 4;
+                        break;
+                    case 1:
+                        //North-East
+                        config = 7;
+                        break;
+                    case 2:
+                        //North-South
+                        config = 15;
+                        break;
+                    case 3:
+                        //North-West
+                        config = 8;
+                        break;
+                    case 5:
+                        //North-South-East
+                        config = 10;
+                        break;
+                    case 6:
+                        //North-South-West
+                        config = 12;
+                        break;
+                    case 9:
+                        //All sides
+                        config = 13;
+                        break;
+                    case 14:
+                        //North-East-West
+                        config = 11;
+                        break;
+                }
+            }
         }
-        var road = new Road().prototype;
-        //All surrounding tiles are road
-        if (tileEast === road && tileSouth === road && tileWest === road && tileNorth === road) {
-            return 1;
-        } else if (tileEast !== road) {
-            //All but East is road
-            return 2;
-        } else if (tileWest !== road) {
-            //All but West is road
-            return 3;
-        } else if (tileNorth !== road) {
-            //All but North is road
-            return 4;
+        window.console.log(end);
+        if (config === 0) {
+            map[x][y] = df;
         } else {
-            return 5;
-            //All but South is road
-        }
-        
-        //North & South are road
-        if (tileNorth === road && tileSouth === road) {
-            return 6;
-        }
-        //East & West are road
-        if (tileEast === road && tileWest === road) {
-            return 7;
-        }
-        
-        //Northeast
-        if (tileNorth === road && tileEast === road) {
-            return 8;
-            //Southeast
-        } else if (tileEast === road && tileSouth === road) {
-            return 9;
-            //Southwest
-        } else if (tileSouth === road && tileWest === road) {
-            return 10;
-            //Northwest
-        } else {
-            return 11;
-        }
-        
-        //North only
-        if (tileNorth === road) {
-            return 12;
-            //East only
-        } else if (tileEast === road) {
-            return 13;
-            //South only
-        } else if (tileSouth === road) {
-            return 14;
-            //West only
-        } else {
-            return 15;
+            
+            switch(df) {
+                case roadType.HORIZONTAL:
+                    switch(config) {
+                        case 2:
+                            //South
+                            df = roadType.THREESOUTH;
+                            break;
+                        case 4:
+                            //North
+                            df = roadType.THREENORTH;
+                            break;
+                        case 5:
+                            //South-East
+                            df = roadType.THREESOUTH;
+                            break;
+                        case 6:
+                            //South-West
+                            df = roadType.THREESOUTH;
+                            break;
+                        case 7:
+                            //North-East
+                            df = roadType.THREENORTH;
+                            break;
+                        case 8:
+                            //North-West
+                            df = roadType.THREENORTH;
+                            break;
+                        case 9:
+                            //South-East-West
+                            df = roadType.THREESOUTH;
+                            break;
+                        case 10:
+                            //North-South-East
+                            df = roadType.THREEEAST;
+                            break;
+                        case 11:
+                            //North-East-West
+                            df = roadType.THREENORTH;
+                            break;
+                        case 12:
+                            //North-South-West
+                            if (end === true) {
+                                df = roadType.THREEWEST;
+                            } else {
+                                df = roadType.FOURWAY;
+                            }
+                            break;
+                        case 13:
+                            //All
+                            df = roadType.FOURWAY;
+                            break;
+                    }
+                    break;
+                case roadType.VERTICAL:
+                    switch(config) {
+                        case 1:
+                            //East
+                            if (end === true) {
+                                df = roadType.SE;
+                            } else {
+                                df = roadType.THREEEAST;
+                            }
+                            break;
+                        case 3:
+                            //West
+                            if (end === true) {
+                                df = roadType.SW;
+                            } else {
+                                df = roadType.THREEWEST;
+                            }
+                            break;
+                        case 5:
+                            //South-East
+                            if (end === true) {
+                                df = roadType.SE;
+                            } else {
+                                df = roadType.THREEEAST;
+                            }
+                            break;
+                        case 6:
+                            //South-West
+                            if (end === true) {
+                                df = roadType.SW;
+                            } else {
+                                df = roadType.THREEWEST;
+                            }
+                            break;
+                        case 7:
+                            //North-East
+                            if (end === true) {
+                                df = roadType.NE;
+                            } else {
+                                df = roadType.THREEEAST;
+                            }
+                            break;
+                        case 8:
+                            //North-West
+                            if (end === true) {
+                                df = roadType.NW;
+                            } else {
+                                window.console.log(end);
+                                df = roadType.THREEWEST;
+                            }
+                            break;
+                        case 9:
+                            //South-East-West
+                            df = roadType.FOURWAY;
+                            break;
+                        case 10:
+                            //North-South-East
+                            df = roadType.THREEEAST;
+                            break;
+                        case 11:
+                            //North-East-West
+                            if (end === true) {
+                                df = roadType.THREENORTH;
+                            } else {
+                                df = roadType.FOURWAY;
+                            }
+                            break;
+                        case 12:
+                            //North-South-West
+                            df = roadType.THREEWEST;
+                            break;
+                        case 13:
+                            //All
+                            df = roadType.FOURWAY;
+                            break;
+                    }
+                    break;
+            }
+            map[x][y] = df;
+            window.console.log(config);
         }
     };
     
@@ -120,20 +282,29 @@ function RoadTool() {
         if (pointA[0] === pointB[0]) {
             for (var i = pointA[0]; i <= pointB[0]; i++) {
                 for (var j = pointA[1]; j <= pointB[1]; j++) {
-                    if (map[i][j] !== 0 || map[i][j] !== 1) {
-                        map[i][j].type = map[i][j].tiles.OCCUPIED;
-                        map[i][j].data = new Road(map[i][j], roadType.VERTICAL);
-                    } else if (map[i][j].data.prototype === new Road().prototype) {
-                        tileCheck = checkTiles(i, j, map);
+                    if (map[i][j] === 0 || map[i][j] === 1) {
+                        map[i][j] = roadType.VERTICAL;
+                    } else {
+                        if ((i === pointB[0] && j === pointB[1]) || (i === pointA[0] && j === pointA[1])) {
+                            checkTiles(i, j, map, roadType.VERTICAL, true);
+                            window.console.log([pointB, [i, j]]);
+                        } else {
+                            checkTiles(i, j, map, roadType.VERTICAL, false);
+                        }
                     }
                 }
             }
         } else {
             for (var h = pointA[0]; h <= pointB[0]; h++) {
                 for (var k = pointA[1]; k <= pointB[1]; k++) {
-                    if (map[h][k].type !== map[h][k].tiles.OCCUPIED) {
-                        map[h][k].type = map[h][k].tiles.OCCUPIED;
-                        map[h][k].data = new Road(map[h][k], roadType.HORIZONTAL);
+                    if (map[h][k] === 0 || map[h][k] === 1) {
+                        map[h][k] = roadType.HORIZONTAL;
+                    } else {
+                       if (h === pointB[0] && k === pointB[1]) {
+                           checkTiles(h, k, map, roadType.HORIZONTAL, true);
+                       } else {
+                           checkTiles(h, k, map, roadType.HORIZONTAL, false); 
+                       }
                     }
                 }
             }
--- js/tile.js
+++ js/tile.js
@@ -2,106 +2,13 @@
 /* jshint -W117 */
 function Tile() {
     
-    /*this.x = sx;
-    this.y = sy;
-    this.spriteX = sx*2;
-    this.spriteY = sy;
-    var x, y;
-    this.data = undefined;
-    
-    this.tiles = {GRASS: 1, STONE: 2, OCCUPIED: 3};*/
-    
     var a = Math.round(Math.random());
     
     if (a === 1) {
         return 0;
     } else if (a === 0) return 1;
     
-    /*this.update = function update() {
-        x = this.x - (Game.Camera.getX()/32);
-        y = this.y - (Game.Camera.getY()/32);
-        if (this.data !== undefined) this.data.update();
-    };
-    
-    this.draw = function draw() {
-        var xd = (x)*32;
-        var yd = (y)*32;
-        var x1 = CartToIso(xd, yd);
-        var border = 32;
-        var screenCheck = CartToIso(xd+16, yd+16);
-        if (screenCheck[0] > border && screenCheck[1] > border && screenCheck[0] < canvas.width-border && screenCheck[1] < canvas.height-border) {
-            
-            switch(this.type) {
-                case this.tiles.GRASS:
-                    return 0;
-                    break;
-                case this.tiles.STONE:
-                    return 1;
-                case this.tiles.OCCUPIED:
-                    //window.console.log(this.data);
-                    this.data.draw();
-            }
-        }
-    };*/
 }
-roadType = {FOURWAY: 1, THREENORTH: 2, THREESOUTH: 3,
-            THREEEAST: 4, THREEWEST: 5, VERTICAL: 6, 
-            HORIZONTAL: 7, SW: 8, SE: 9, NW: 10, NE: 11};
-/* function Road(tile, type) {
-    
-    var place = tile;
-    var x, y;
-    this.type = type;
-    
-    this.update = function update() {
-        x = place.x - (Game.Camera.getX()/32);
-        y = place.y - (Game.Camera.getY()/32);
-    };
-    
-    this.draw = function draw() {
-        var xd = (x)*32;
-        var yd = (y)*32;
-        var x1 = CartToIso(xd, yd);
-
-        var screenCheck = CartToIso(xd+16, yd+16);
-        if (screenCheck[0] >= 32 && screenCheck[1] >= 32 && screenCheck[0] <= canvas.width-32 && screenCheck[1] <= canvas.height-32) {
-            
-            switch(this.type) {
-                case roadType.FOURWAY:
-                    context.drawImage(Game.spriteSheet, 0, 32, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.THREENORTH:
-                    context.drawImage(Game.spriteSheet, 63, 64, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.THREESOUTH:
-                    context.drawImage(Game.spriteSheet, 126, 96, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.THREEEAST:
-                    context.drawImage(Game.spriteSheet, 126, 64, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.THREEWEST:
-                    context.drawImage(Game.spriteSheet, 63, 96, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.VERTICAL: 
-                    context.drawImage(Game.spriteSheet, 126, 32, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.HORIZONTAL:
-                    context.drawImage(Game.spriteSheet, 63, 32, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.SW:
-                    context.drawImage(Game.spriteSheet, 0, 64, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.SE:
-                    context.drawImage(Game.spriteSheet, 189, 64, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.NW:
-                    context.drawImage(Game.spriteSheet, 189, 32, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-                case roadType.NE:
-                    context.drawImage(Game.spriteSheet, 0, 96, 64, 32, Math.floor(x1[0]-32), x1[1], 64, 32);
-                    break;
-            }
-        }
-    };
-    
-} */
+roadType = {FOURWAY: 2, THREENORTH: 3, THREESOUTH: 4,
+            THREEEAST: 5, THREEWEST: 6, VERTICAL: 7, 
+            HORIZONTAL: 8, SW: 9, SE: 10, NW: 11, NE: 12};
